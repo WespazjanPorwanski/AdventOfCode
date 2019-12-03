e2d587849d47e94dfeca65d27829aaec41bc0ef7
wespazjan.porwanski <wespazjan.porwanski@asseco.pl>
2019-12-03 19:43:04 +0100

    Day3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import day2.Day2Part2;
+import day3.Day3;
 
 public class Main {
 
@@ -6,6 +7,6 @@ public class Main {
 //        Day1Part1.main();
 //        Day1Part2.main();
 //        Day2Part1.main();
-        Day2Part2.main();
+        Day3.main();
     }
 }
